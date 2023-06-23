8fa57c3a34942a618daef7883b75d8a7bb7a331d
syp0812 <94878158+syp0812@users.noreply.github.com>
2023-06-23 19:46:40 +0900

    Update CorsConfig.java

--- core/src/main/java/com/example/talken/common/config/CorsConfig.java
+++ core/src/main/java/com/example/talken/common/config/CorsConfig.java
@@ -1,8 +1,14 @@
 package com.example.talken.common.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
@@ -11,10 +17,28 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*://*")
+                .allowedOrigins("http://43.202.3.11:3000")
+                .allowedOrigins("http://43.202.3.11:8080")
+                .allowedOrigins("https://talken.moim.link:3000")
+                .allowedOrigins("https://talken.moim.link:8080")
                 .allowedMethods("*")
                 .allowedHeaders("*")
-//                .allowCredentials(true)
+                .allowCredentials(false)
                 .maxAge(3000);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD","POST","GET","DELETE","PUT"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
