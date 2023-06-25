86bc91658923257a0eab7f2dbd8a76a59e75c92d
syp0812 <94878158+syp0812@users.noreply.github.com>
2023-06-25 15:13:44 +0900

    Resume 테이블에서 Keyword 테이블 분리

--- core/src/main/java/com/example/talken/resume/dto/ResumeRequestDto.java
+++ core/src/main/java/com/example/talken/resume/dto/ResumeRequestDto.java
@@ -1,16 +1,29 @@
 package com.example.talken.resume.dto;
 
+import com.example.talken.common.Status;
 import com.example.talken.resume.entity.Resume;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ResumeRequestDto {
 
-    public Resume toEntity() {
+    private String parentCategory;
+    private String childCategory;
+    private List<String> keywords;
+    private String imageURl;
+
+    public Resume toEntity(Status.Resume publicStatus, Status.Feedback feedbackStatus) {
         return Resume.builder()
+                .parentCategory(parentCategory)
+                .childCategory(childCategory)
+                .imageURl(imageURl)
+                .publicStatus(publicStatus)
+                .feedbackStatus(feedbackStatus)
                 .build();
     }
 }
--- core/src/main/java/com/example/talken/resume/service/ResumeService.java
+++ core/src/main/java/com/example/talken/resume/service/ResumeService.java
@@ -1,12 +1,10 @@
 package com.example.talken.resume.service;
 
+import com.example.talken.common.Status;
 import com.example.talken.resume.dto.ResumeRequestDto;
 import com.example.talken.resume.dto.ResumeResponseDto;
 import com.example.talken.resume.entity.Resume;
-import com.example.talken.resume.exception.ResumeError;
-import com.example.talken.resume.exception.ResumeException;
 import com.example.talken.resume.repository.ResumeRepository;
-import com.example.talken.user.entity.User;
 import com.example.talken.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,6 +13,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ResumeService {
 
+    private static Status.Resume publicStatus = Status.Resume.PUBLIC;
+    private static Status.Feedback feedbackStatus = Status.Feedback.WANTED;
+
+
     private final ResumeRepository resumeRepository;
     private final UserRepository userRepository;
 
@@ -22,7 +24,7 @@ public class ResumeService {
 //        userRepository.findByEmail(user.getEmail())
 //                .orElseThrow(() -> new ResumeException(ResumeError.INVALID_USER));
 
-        Resume resume = resumeRepository.save(request.toEntity());
+        Resume resume = resumeRepository.save(request.toEntity(publicStatus, feedbackStatus));
 
         return ResumeResponseDto.fromEntity(resume);
     }
