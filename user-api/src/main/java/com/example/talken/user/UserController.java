193ddf260bec67b31423541e06586c6613794c8c
syp0812 <94878158+syp0812@users.noreply.github.com>
2023-06-28 22:44:48 +0900

    feat: Spring Security, OAuth2를 이용하여 카카오 로그인을 구현한다.

--- user-api/src/main/java/com/example/talken/user/UserController.java
+++ user-api/src/main/java/com/example/talken/user/UserController.java
@@ -1,24 +1,31 @@
 package com.example.talken.user;
 
 import com.example.talken.common.Response;
+import com.example.talken.user.dto.TokenDto;
 import com.example.talken.user.dto.UserRequestDto;
 import com.example.talken.user.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("/signup")
+    @GetMapping("/user/kakao/callback")
+    public ResponseEntity<Response<TokenDto>> kakaoLogin(@RequestParam(value = "code") String code) {
+        return ResponseEntity
+                .status(HttpStatus.OK.value())
+                .body(Response.<TokenDto>builder()
+                        .data(userService.kakaoLogin(code))
+                        .build());
+    }
+
+    @PostMapping("/users/signup")
     public Response<Void> signup(@RequestBody UserRequestDto.Signup request) {
         userService.signup(request);
 
@@ -27,7 +34,7 @@ public class UserController {
                 .build();
     }
 
-    @PostMapping("/login")
+    @PostMapping("/users/login")
     public Response<Void> login(@RequestBody UserRequestDto.Login request, HttpServletResponse response) {
         userService.login(request, response);
 
