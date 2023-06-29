77f738aa9c5037a29a2857202050824b736af29d
syp0812 <94878158+syp0812@users.noreply.github.com>
2023-06-30 01:12:26 +0900

    feat: 이력서 수정 시 연관 엔티티도 함께 수정되도록 구현한다.

--- core/src/main/java/com/example/talken/resume/dto/request/ResumeRequestDto.java
+++ core/src/main/java/com/example/talken/resume/dto/request/ResumeRequestDto.java
@@ -2,21 +2,22 @@ package com.example.talken.resume.dto.request;
 
 import com.example.talken.common.Status;
 import com.example.talken.experience.dto.ExperienceRequestDto;
-import com.example.talken.experience.entity.Experience;
 import com.example.talken.resume.entity.Resume;
 import com.example.talken.user.entity.User;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ResumeRequestDto {
-
+    @NotNull
     private String parentCategory;
+
+    @NotNull
     private String childCategory;
+
     private ExperienceRequestDto experienceRequest;
 
     public Resume toEntity(Status.Resume publicStatus, Status.Feedback feedbackStatus, User user) {
