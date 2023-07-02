fcc2b6cc5dfa9750997e4ebf409ade1882cdf351
Soyeon Park <syp08120@gmail.com>
2023-07-03 01:13:04 +0900

    Revert "feature/swagger"

--- user-api/src/main/java/com/example/talken/feedback/FeedbackController.java
+++ user-api/src/main/java/com/example/talken/feedback/FeedbackController.java
@@ -6,16 +6,12 @@ import com.example.talken.feedback.dto.request.FeedbackRequestDto;
 import com.example.talken.feedback.dto.response.FeedbackListResponseDto;
 import com.example.talken.feedback.dto.response.FeedbackResponseDto;
 import com.example.talken.feedback.service.FeedbackService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "feedback", description = "피드백 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/resumes/feedbacks")
@@ -23,12 +19,10 @@ public class FeedbackController {
 
     private final FeedbackService feedbackService;
 
-    @Operation(summary = "피드백 작성")
-    @Parameter(name = "resumeId", description = "피드백을 작성할 이력서의 id")
     @PostMapping("/{resumeId}")
     public ResponseEntity<Response<FeedbackResponseDto>> createFeedback(@PathVariable Long resumeId,
-                                                                        @RequestBody FeedbackRequestDto requestDto,
-                                                                        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+                                                        @RequestBody FeedbackRequestDto requestDto,
+                                                        @AuthenticationPrincipal UserDetailsImpl userDetails) {
         feedbackService.createFeedback(resumeId, requestDto, userDetails);
 
         return ResponseEntity
@@ -38,11 +32,9 @@ public class FeedbackController {
                         .build());
     }
 
-    @Operation(summary = "이력서별 피드백 조회", description = "이력서 id를 이용하여 피드백 조회")
-    @Parameter(name = "resumeId")
     @GetMapping("/{resumeId}")
     public ResponseEntity<Response<FeedbackListResponseDto>> readFeedbackList(@PathVariable Long resumeId,
-                                                                              @AuthenticationPrincipal UserDetailsImpl userDetails) {
+                                                              @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return ResponseEntity
                 .status(HttpStatus.OK.value())
                 .body(Response.<FeedbackListResponseDto>builder()
--- user-api/src/main/java/com/example/talken/user/UserController.java
+++ user-api/src/main/java/com/example/talken/user/UserController.java
@@ -4,25 +4,20 @@ import com.example.talken.common.Response;
 import com.example.talken.user.dto.TokenDto;
 import com.example.talken.user.dto.UserRequestDto;
 import com.example.talken.user.service.UserService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "user", description = "사용자 API")
 @RestController
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
-    @Operation(summary = "카카오 소셜 로그인")
     @GetMapping("/user/kakao/callback")
-    public ResponseEntity<Response<TokenDto>> kakaoLogin(@Parameter(name = "code") @RequestParam(value = "code") String code) {
+    public ResponseEntity<Response<TokenDto>> kakaoLogin(@RequestParam(value = "code") String code) {
         return ResponseEntity
                 .status(HttpStatus.OK.value())
                 .body(Response.<TokenDto>builder()
